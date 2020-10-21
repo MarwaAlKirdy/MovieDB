b306863e5b0e17fa259c49fbd31c9443ab930642
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-10-21 13:15:06 +0300

    Step_3

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+var today = new Date();
+var hours = today.getHours();
+var minutes = today.getMinutes();
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}
+var time = hours + ":" + minutes;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.get('/', function (req, res) {
     res.send("ok");
+});
+
+app.get("/test", function (req, res) {
+    res.send({
+        status: 200,
+        message: "ok"
+    });
+});
+
+app.get("/time", function (req, res) {
+
+    res.send("{status: 200, message: " + time + "}");
 });
