d01186e433547173dd6d5df070dab6d6cf78d684
Marwa Al Kirdy <marwaalkirdy96@gmail.com>
2020-10-21 15:02:20 +0300

    Step_4

--- index.js
+++ index.js
@@ -5,25 +5,36 @@ var today = new Date();
 var hours = today.getHours();
 var minutes = today.getMinutes();
 if (minutes < 10) {
-    minutes = "0" + minutes;
+  minutes = "0" + minutes;
 }
 var time = hours + ":" + minutes;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get('/', function (req, res) {
-    res.send("ok");
+app.get("/", function (req, res) {
+  res.send("ok");
 });
 
 app.get("/test", function (req, res) {
-    res.send({
-        status: 200,
-        message: "ok"
-    });
+  res.status(200).send("{message: ok}");
 });
 
 app.get("/time", function (req, res) {
+  res.status(200).send("{message: " + time + "}");
+});
 
-    res.send("{status: 200, message: " + time + "}");
+app.get("/hello", function (req, res) {
+  res.status(200).send("Hello!");
+});
+app.get("/hello/:id", function (req, res) {
+  res.status(200).send("Hello, " + req.params.id);
+});
+
+app.get("/search", function (req, res) {
+  if (req.query.s == "" || typeof req.query.s === "undefined") {
+    res.status(500).send("{error: true message: you have to provide a search}");
+  } else {
+    res.status(200).send("{message: ok data: " + req.query.s + "}");
+  }
 });
